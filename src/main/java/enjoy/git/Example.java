5fa907cb3de6128856282ee2d5f96a1817db777c
Joseph-jun <596213836@qq.com>
2021-03-05 15:05:10 +0800

    new git

---
+++ src/main/java/enjoy/git/Example.java
@@ -0,0 +1,26 @@
+package enjoy.git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : Joseph.Tan
+ * @date : 2021/3/5 14:29
+ */
+@RestController
+@EnableAutoConfiguration
+//@SpringBootApplication
+public class Example {
+
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello Git!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Example.class,args);
+    }
+}
